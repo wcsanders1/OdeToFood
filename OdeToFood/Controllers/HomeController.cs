744c62edd2a1f798bebcad655604767d4c3271ab
William Sanders <william.sanders.dev@gmail.com>
2016-11-05 20:45:42 -0400

    template

---
+++ OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            var model = new AboutModel();
+            model.Name = "Bill";
+            model.Location = "Grand Rapids";
+
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
