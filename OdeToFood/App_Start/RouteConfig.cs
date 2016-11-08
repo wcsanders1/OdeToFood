29dfe39598ea8cd2e9416892957cbac741a2058c
William Sanders <william.sanders.dev@gmail.com>
2016-11-07 21:56:58 -0500

    added controller and alternative route

--- OdeToFood/App_Start/RouteConfig.cs
+++ OdeToFood/App_Start/RouteConfig.cs
@@ -13,6 +13,10 @@ namespace OdeToFood
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Cuisine",
+                "cuisine/{name}",
+                new { controller = "Cuisine", action = "Search", name = UrlParameter.Optional });
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -11,6 +11,14 @@ namespace OdeToFood.Controllers
     {
         public ActionResult Index()
         {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            var id = RouteData.Values["id"];
+
+            var message = String.Format("{0}::{1} {2}", controller, action, id);
+
+            ViewBag.Message = message;
+
             return View();
         }
 
