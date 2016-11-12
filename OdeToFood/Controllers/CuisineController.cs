47fdfd767512ea4c7887120ce0dd128442b559f0
William Sanders <william.sanders.dev@gmail.com>
2016-11-11 22:21:40 -0500

    added log attribute and review view

--- OdeToFood/Controllers/CuisineController.cs
+++ OdeToFood/Controllers/CuisineController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OdeToFood.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +9,16 @@ namespace OdeToFood.Controllers
 {
     public class CuisineController : Controller
     {
-        // GET: Cuisine
+        //[Authorize]
+        [Log]
         public ActionResult Search(string name)
         {
+            throw new Exception("Something terrible has happened");
+
             var message = Server.HtmlEncode(name);
 
             return Content(message);
         }
+
     }
 }
