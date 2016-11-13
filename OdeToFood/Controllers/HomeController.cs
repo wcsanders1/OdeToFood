dba8ef81f5b6c40697b0e47c4d32a6ddf7936af2
William Sanders <william.sanders.dev@gmail.com>
2016-11-13 12:38:18 -0500

    added DbContext

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -9,17 +9,38 @@ namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        OdeToFoodDb _db = new OdeToFoodDb();
+        public ActionResult Index(string searchTerm = null)
         {
-            var controller = RouteData.Values["controller"];
-            var action = RouteData.Values["action"];
-            var id = RouteData.Values["id"];
+            //var model =
+            //    from r in _db.Restaurants
+            //    orderby r.Reviews.Average(review => review.Rating) descending
+            //    select new RestaurantListViewModel
+            //    {
+            //        Id = r.Id,
+            //        Name = r.Name,
+            //        City = r.City,
+            //        Country = r.Country,
+            //        CountOfReviews = r.Reviews.Count()
+            //    };
 
-            var message = String.Format("{0}::{1} {2}", controller, action, id);
+            //Above and below work equally well here except only in below can you use .Take(10)
 
-            ViewBag.Message = message;
+            var model =
+                _db.Restaurants
+                .OrderByDescending(r => r.Reviews.Average(review => review.Rating))
+                .Where(r => searchTerm == null || r.Name.StartsWith(searchTerm))
+                .Take(10)
+                .Select(r => new RestaurantListViewModel
+                        { 
+                            Id = r.Id,
+                            Name = r.Name,
+                            City = r.City,
+                            Country = r.Country,
+                            CountOfReviews = r.Reviews.Count()
+                        });
 
-            return View();
+            return View(model);
         }
 
         public ActionResult About()
@@ -36,6 +57,15 @@ namespace OdeToFood.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ OdeToFood/Migrations/Configuration.cs
@@ -0,0 +1,49 @@
+namespace OdeToFood.Migrations
+{
+    using Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<OdeToFood.Models.OdeToFoodDb>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            ContextKey = "OdeToFood.Models.OdeToFoodDb";
+        }
+
+        protected override void Seed(OdeToFood.Models.OdeToFoodDb context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+
+            context.Restaurants.AddOrUpdate(r => r.Name,
+                new Restaurant { Name = "Sabatino's", City = "Baltimore", Country = "USA" },
+                new Restaurant { Name = "Great Lake", City = "Chicago", Country = "USA" },
+                new Restaurant
+                {
+                    Name = "Smaka",
+                    City = "Gothenburg",
+                    Country = "Sweden",
+                    Reviews =
+                    new List<RestaurantReview>
+                    {
+                        new RestaurantReview {Rating = 9, Body = "Great Food!", ReviewerName = "Scott" }
+                    }
+                });
+        }
+    }
+}
